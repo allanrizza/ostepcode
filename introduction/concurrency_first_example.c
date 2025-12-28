a720c7d77a952ab7ebbac1316e284a7583a3b837
Allan Rizza <allanrizza.dev@gmail.com>
2025-12-28 01:09:06 -0300

    concurrency first example

---
+++ introduction/concurrency_first_example.c
@@ -0,0 +1,37 @@
+//
+// Created by Allan Rizza on 12/28/2025.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "common_threads.h"
+
+volatile int counter = 0;
+int loops;
+
+void *worker(void *arg) {
+    int i;
+    for (i = 0; i < loops; i++) {
+        counter++;
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: threads <value>\n");
+        exit(1);
+    }
+
+    loops = atoi(argv[1]); //ASCII to integer
+    pthread_t p1, p2;
+    printf("Initial value: %d\n", counter);
+
+    Pthread_create(&p1, NULL, worker, NULL);
+    Pthread_create(&p2, NULL, worker, NULL);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value: %d\n", counter);
+    return 0;
+}
